3173e8dd6e44924c38085b291deaed0f521c6c1d
danielvishna <73115554+danielvishna@users.noreply.github.com>
2023-03-14 19:25:13 +0200

    all my stuaff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component, useState } from 'react';
 // import Elevator from './Elevator';
-import {motion} from "framer-motion";
+import {delay, motion} from "framer-motion";
 import elevatorSvg from './elevator.svg';
+function Elevator(props) {
+  return (
+    <motion.div
+      animate={{ y: [0 ,props.currentFloor * -33, 0], x: -40}}
+      transition={{ duration: props.currentFloor, type: "tween"}}
+    >
+      <img className='img' src={elevatorSvg} alt="Elevator" />
+    </motion.div>
+  );
+}
 
-
-export default function Building(){
-   // const { floors } = this.state;
+function Board(){
   const boardRows = [];
-  // const [doMove, setDoMove] = useState(false);
   const [currentFloor, setCurrentFloor] = useState(0);
+  // const [doMove, setDoMove] = useState(false);
+
   //Todo: change all the keys
   for (let i = 9; i >= 0; i--) {
     const boardColumns = [];
@@ -27,29 +36,39 @@ export default function Building(){
     else{
       boardColumns.push(<div key={i} className="floor-num">{i}th</div>);
     }
-    for (let j = 0; j < 1; j++) {
+    for (let j = 0; j < 3; j++) {
       boardColumns.push(
           <div key={i*5 + j + 100} className="square">{}</div>
         );
-        if(i === 0){
-          boardColumns.push(<motion.div key={700 + j}
-          animate={{y :  currentFloor * -32, x: -40}} 
-          transition = {{type : "tween"}}
-          >
-           <img className='img' src={elevatorSvg} alt="Elevator" />
-          </motion.div>);
+        if(i === 0 && (j === 2|| j === 0)){
+          boardColumns.push(<Elevator key={j + 78} currentFloor={currentFloor}/>);
         }
       
 
     }
-    boardColumns.push(<button key={i + 200} className='call'  onClick={() => setCurrentFloor(i)}>Call</button>);
+    boardColumns.push(<button key={i + 200} className='call'  onClick={() => {
+      setCurrentFloor(i);
+      
+    }
+    }>Call</button>);
     boardRows.push(<div key={i + 300} className="board-row">{boardColumns}</div>);
   }
-    return (
-      <>
+  return boardRows
+}
+
+
+export default function Building(){
+
+  // const { floors } = this.state;
+  const [elevatorMoving, setElevatorMoving] = useState([Array(5).fill(false)]);
+  const [elevatorFloor, setElevatorFloor] = useState([Array(5).fill(0)]);
+  const [buttonCall, setButtonCall] = useState([Array(10).fill(false)]);
+  
+  return (
+    <>
       <h1 key={900}>Elevator Exercise</h1>
       <div key={800} className="building">
-        {boardRows}
+        <Board/>
       </div>
       </>
     );
