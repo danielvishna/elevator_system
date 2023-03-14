6362226501ac315127305f892d2d817cc2087ec3
danielvishna <73115554+danielvishna@users.noreply.github.com>
2023-03-14 15:04:32 +0200

    commit before try some thing new

--- src/App.js
+++ src/App.js
@@ -1,76 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 // import Elevator from './Elevator';
 import {motion} from "framer-motion";
 import elevatorSvg from './elevator.svg';
 
 
-class Building extends Component {
-  state = {
-    floors: [
-      { id: 10, calling: false },
-      { id: 9, calling: false },
-      { id: 8, calling: false },
-      { id: 7, calling: false },
-      { id: 6, calling: false },
-      { id: 5, calling: false },
-      { id: 4, calling: false },
-      { id: 3, calling: false },
-      { id: 2, calling: false },
-      { id: 1, calling: false }, // ground floor
-    ],
-  };
-  
-
-  callElevator = (floorId) => {
-    // Logic to call an elevator to a specific floor goes here
-  };
-  
-
-  render() {
-    // const { floors } = this.state;
+export default function Building(){
+   // const { floors } = this.state;
   const boardRows = [];
-
+  // const [doMove, setDoMove] = useState(false);
+  const [currentFloor, setCurrentFloor] = useState(0);
+  //Todo: change all the keys
   for (let i = 9; i >= 0; i--) {
     const boardColumns = [];
     if(i===0){
-      boardColumns.push(<h4 className="floor-num">Ground Floor</h4>);
+      boardColumns.push(<div key={i} className="floor-num">Ground Floor</div>);
     }
     else if(i===1){
-      boardColumns.push(<div className="floor-num">{i}st</div>);
+      boardColumns.push(<div key={i} className="floor-num">{i}st</div>);
     }
     else if(i===2){
-      boardColumns.push(<div className="floor-num">{i}nd</div>);
+      boardColumns.push(<div key={i} className="floor-num">{i}nd</div>);
     }
     else if(i===3){
-      boardColumns.push(<div className="floor-num">{i}rd</div>);
+      boardColumns.push(<div key={i} className="floor-num">{i}rd</div>);
     }
     else{
-      boardColumns.push(<div className="floor-num">{i}th</div>);
+      boardColumns.push(<div key={i} className="floor-num">{i}th</div>);
     }
-    for (let j = 0; j < 5; j++) {
-      if(i === 0){
-        boardColumns.push(<motion.div>
-         <img className='img' src={elevatorSvg} alt="Elevator" />
-        </motion.div>);
-      }
+    for (let j = 0; j < 1; j++) {
       boardColumns.push(
-          <div className="square">{}</div>
+          <div key={i*5 + j + 100} className="square">{}</div>
         );
+        if(i === 0){
+          boardColumns.push(<motion.div key={700 + j}
+          animate={{y :  currentFloor * -32, x: -40}} 
+          transition = {{type : "tween"}}
+          >
+           <img className='img' src={elevatorSvg} alt="Elevator" />
+          </motion.div>);
+        }
       
 
     }
-    boardColumns.push(<button className='call'>Call</button>);
-    boardRows.push(<div className="board-row">{boardColumns}</div>);
+    boardColumns.push(<button key={i + 200} className='call'  onClick={() => setCurrentFloor(i)}>Call</button>);
+    boardRows.push(<div key={i + 300} className="board-row">{boardColumns}</div>);
   }
     return (
       <>
-      <h1>Elevator Exercise</h1>
-      <div className="building">
+      <h1 key={900}>Elevator Exercise</h1>
+      <div key={800} className="building">
         {boardRows}
       </div>
       </>
     );
   }
-}
 
-export default Building;
+
+
